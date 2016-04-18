8c10bf32591500f251a4c7e0cfa69ab767b1a29e
peveloper <stefano.peverelli@usi.ch>
2016-04-18 22:19:46 +0200

    Implemented patch extractor method.

--- scripts/patch.py
+++ scripts/patch.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python
 import sys, getopt
-from PIL import Image
 import numpy as np
+import cv2
 import csv
-
-def rotate(inputfile, outputfile):
-    try:
-        image = Image.open(inputfile)
-    except:
-        print "Wrong path or unsupported file extension."
-    image = image.rotate(45, expand=True)
-    image.show()
-    return image.save(outputfile)
+import math
 
 
-def midpoint(v):
-    return (v[1:] + v[:-1]) / 2
+def midpoint(x0, y0, x1, y1):
+    return [(x0 + x1) / 2, (y0 + y1) / 2]
 
 
-def signed_angle(v0,v1):
-    angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
-    return np.degrees(angle)
+def get_distance(x0, y0, x1, y1):
+    return int(math.sqrt((y1 - y0) ** 2 + (x1 - x0) ** 2))
 
 
 def remove_quotes(field):
@@ -29,12 +20,24 @@ def remove_quotes(field):
         field = field.replace(' ', '')
     return field
 
+
 def to_pixel_coords(field, mul):
     return int(field * mul) if (field > 0) else int(field * (-mul))
 
 
-def parse_record(row, width, height):
+def extract_patch(image, center, theta, width, height):
+    vx = (np.cos(theta), np.sin(theta))
+    vy = (-np.sin(theta), np.cos(theta))
+    sx = center[0] - vx[0] * (width / 2) - vy[0] * (height / 2)
+    sy = center[1] - vx[1] * (width / 2) - vy[1] * (height / 2)
+
+    mapping = np.array([[vx[0],vy[0], sx], [vx[1],vy[1], sy]])
+    return cv2.warpAffine(image, mapping, (width, height), flags = cv2.WARP_INVERSE_MAP, borderMode = cv2.BORDER_REPLICATE)
+
+
+def parse_record(row, line, width, height, img, output_img):
     row = [remove_quotes(field) for field in row ]
+
     for i in range(len(row)):
         if i < 2:
             continue
@@ -42,22 +45,43 @@ def parse_record(row, width, height):
             row[i] = to_pixel_coords(float(row[i]), width)
         else:
             row[i] = to_pixel_coords(float(row[i]), height)
+
+    p0 = (row[2], row[3])
+    p1 = (row[4], row[5])
+    traversed = int(row[0])
+    angle = math.atan2(p1[1] - p0[1], p1[0] - p0[0])
+
+    if (angle < 0):
+        angle += np.pi / 2
+
+    center = midpoint(p0[0], p0[1], p1[0], p1[1])
+    distance = get_distance(center[0], center[1], p0[0], p0[1])
+    patch = extract_patch(img, (center[0], center[1]), angle, distance * 2, distance * 2)
+
+    cv2.imwrite('../data/patches/patch_' + `line - 2` + '.jpg', patch)
+    patch = cv2.imread('../data/patches/patch_' + `line - 2` + '.jpg', cv2.IMREAD_GRAYSCALE)
+    color = cv2.COLORMAP_SUMMER if traversed else cv2.COLORMAP_AUTUMN
+    patch_col = cv2.applyColorMap(patch, color)
+    cv2.imwrite('../data/patches/patch_' + `line - 2` + '.jpg', patch_col);
+
     return row
 
 
-def get_image_size(image_reader):
-    width, height = image_reader.size
-    return (width, height)
+def get_image_size(img):
+    height, width, channels = img.shape
+    return (width, height, channels)
 
 
-def get_image(input_img):
-    return Image.open(input_img)
+def get_image(img_path):
+    return cv2.imread(img_path)
 
 
 def main(argv):
-    csv_file = ""
-    input_img = ""
-    output_img = ""
+
+    csv_file = ''
+    input_img = ''
+    output_img = ''
+
     try:
         opts, args = getopt.getopt(argv, "hc:i:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
@@ -74,38 +98,19 @@ def main(argv):
         elif opt in ("-p", "--patch"):
             output_img = arg
 
-    input_img_reader = get_image(input_img)
-    input_img_size = get_image_size(input_img_reader)
-    input_img_width = input_img_size[0]
-    input_img_height = input_img_size[1]
-
-    print input_img_size[0], input_img_size[1]
+    img = get_image(input_img)
+    img_size = get_image_size(img)
+    img_width = img_size[0]
+    img_height = img_size[1]
 
     with open(csv_file, 'rb') as f:
         reader = csv.reader(f)
-        line = 2
+        line = 0
         for row in reader:
-            if line == 0:
-                row = parse_record(row, input_img_width, input_img_height)
-            print row
-            line-= 1
-            # then find the midpoint and the patch coords,
-            # then rotate, crop colour and construct a new image placing the patch in its corresponding coords.
+            if line > 1:
+                row = parse_record(row, line, img_width, img_height, img, output_img)
+            line +=1
 
-
-    # p0 = [0,-1]
-    # p1 = [1,0]
-    # v0 = np.array(p0)
-    # v1 = np.array(p1)
-    # print(signed_angle(v0,v1))
-    # print my_data
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-
-# Need to specify the path of the directory that will contain the heightmaps, pass the name of the heigthmap as parameter
-
-# Perform a rotate/translation of the patch
-
-# Also need to connect to the numpy db to see coords and if the patch was traversed or not
--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,9 +1,9 @@
-if [ -f ../data/results.csv ]
+if [ -f ../data/results/results.csv ]
   then
     echo "results.csv already exists."
   else
-      echo "TRAVERSED, ELAPSED_TIME, P0, P1, P2\n" > ../data/results.csv
+      echo "TRAVERSED, ELAPSED_TIME, P0, P1, P2\n" > ../data/results/results.csv
 fi
 
 cd /Users/stefanopeverelli/Downloads/V-REP_PRO_EDU_V3_3_0_Mac
-./vrep.app/Contents/MacOS/vrep -s -g3 ~/Documents/usi/6ths/Bachelor\ Project/scenes/10x10x1.25_terrain.ttt
+./vrep.app/Contents/MacOS/vrep -h -q -s -g3 ~/Documents/usi/6ths/Bachelor\ Project/scenes/10x10x1.25_terrain.ttt
